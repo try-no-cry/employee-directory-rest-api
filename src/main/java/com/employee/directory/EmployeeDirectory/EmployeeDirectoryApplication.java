bb02602c5b872ae3890bce757a1cb412977cc841
try-no-cry <iabhayt@gmail.com>
2023-03-19 00:33:42 +0530

    adding-spring-intializer-files

---
+++ src/main/java/com/employee/directory/EmployeeDirectory/EmployeeDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.employee.directory.EmployeeDirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employee/directory/EmployeeDirectory/EmployeeDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.directory.EmployeeDirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
