d79dde3918c61c0a2eaac55e5aa38eeaff19073a
try-no-cry <iabhayt@gmail.com>
2023-03-21 13:10:40 +0530

    Added new employee form button

--- src/main/java/com/employee/directory/EmployeeDirectory/Controller/EmployeeRestController.java
+++ src/main/java/com/employee/directory/EmployeeDirectory/Controller/EmployeeRestController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,37 +18,38 @@ import org.springframework.web.bind.annotation.RestController;
 import com.employee.directory.EmployeeDirectory.Entity.Employee;
 import com.employee.directory.EmployeeDirectory.Service.EmployeeService;
 
-//@RestController rest controller when apia
+//@RestController rest controller when api
 /// when thymeleaf use controller
 @Controller
-@RequestMapping("/api")
+@RequestMapping("/employees")
 public class EmployeeRestController {
 	
 	@Autowired
 	private EmployeeService employeeService;
 	
-	@GetMapping("/")
-	public String hi()
+	@GetMapping("/hi")
+	public String hi(Model model)
 	{
+		model.addAttribute("theDate",new Date());
 		return "helloworld";
 	}
 	
-	@GetMapping("/employees")
-	public List<Employee> findAllEmployees()
-	{
-		System.out.println("Inside List<Employee> findAllEmployees()");
-		return employeeService.findAll();
-	}
+//	@GetMapping("/")
+//	public List<Employee> findAllEmployees()
+//	{
+//		System.out.println("Inside List<Employee> findAllEmployees()");
+//		return employeeService.findAll();
+//	}
 	
-	@GetMapping("/list")
+	@GetMapping("/")
 	public String listEmployees(Model model)
 	{
 		System.out.println("Inside listEmployees(Model model)");
 		model.addAttribute("employees",employeeService.findAll());
-		return "list-employees";
+		return "employees/list-employees";
 	}
 	
-	@GetMapping("/employees/{employeeId}")
+	@GetMapping("/{employeeId}")
 	public Employee getEmployeeById(@PathVariable int employeeId)
 	{
 		System.out.println("Inside getEmployeeById(@PathVariable int employeeId)");
@@ -62,7 +64,7 @@ public class EmployeeRestController {
 		return emp;
 	}
 	
-	@PostMapping("/employees")
+	@PostMapping("/")
 	public Employee addEmployee(@RequestBody Employee employee)
 	{
 		employee.setId(0);// putting id 0 incase someone has already provided id....now it will create new emp
@@ -72,14 +74,26 @@ public class EmployeeRestController {
 		
 	}
 	
-	@PutMapping("/employees")
+	@PostMapping("/save")
+	public String saveEmployeeFormData(@ModelAttribute("employee") Employee employee)
+	{
+		employee.setId(0);// putting id 0 incase someone has already provided id....now it will create new emp
+		Employee emp=employeeService.save(employee);
+		
+		return "redirect:/employees/";
+		
+	}
+	
+	
+	
+	@PutMapping("/")
 	public Employee updateEmployee(@RequestBody Employee employee)
 	{
 		Employee emp=employeeService.save(employee);
 		return emp;
 	}
 	
-	@DeleteMapping("/employees/{employeeId}")
+	@DeleteMapping("/{employeeId}")
 	public String deleteEmployeeById(@PathVariable int employeeId)
 	{
 		Employee emp=employeeService.findEmployeeById(employeeId);
@@ -92,6 +106,15 @@ public class EmployeeRestController {
 		employeeService.deleteById(employeeId);
 		
 		return "Employee deleted with id:"+employeeId;
+	}
+	
+	@GetMapping("/addEmployeeForm")
+	public String addNewEmployee(Model model)
+	{
+		Employee emp=new Employee();
+		model.addAttribute(emp);
+		
+		return "employees/addEmployeeForm";
 	}
 	
 }
